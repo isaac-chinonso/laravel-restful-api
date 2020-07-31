d10ac875420d3759172431b5d80caaea6c14df41
Isaac Chinonso Gift <isaacchinonsogift@gmail.com>
2020-07-31 11:34:37 +0100

    updated api

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\http\Requests;
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get article
+        $articles = Article::paginate(15);
+
+        //return collection of articles as a resource
+        return ArticleResource::collection($articles);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\user  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\user  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
